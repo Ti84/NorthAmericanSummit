760ea2fa11489bac3d87d26375a581e56980ea6a
Shane4666 <sc897@nau.edu>
2018-01-15 13:28:26 -0700

    Initial Commit

---
+++ ContactUs.php
@@ -0,0 +1,109 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>North American Summit | ContactUs</title>
+   <link rel="shortcut icon" href="/Images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/Images/favicon.ico" type="image/x-icon">
+    <link href="/Styles/bootstrap.css" rel="stylesheet" />
+    <link href="/Styles/general.css" rel="stylesheet" />
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="/Styles/bootstrap.js"></script>
+     <style>
+	     #tophead{
+                align-content:center;
+				height: 93px;
+				padding-top: 1px;
+            }
+	  #footer{
+            width:100%;
+            bottom:0;
+            left:0;
+        }
+        #supportimg{
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+            height:50px;
+        }
+      td{
+            padding-bottom: 1em !important;
+            padding-left: 10px;
+        }
+					.navbar .navbar-nav {
+			display: inline-block;
+			float: none;
+			vertical-align: top;
+		}
+
+.navbar .navbar-collapse {
+  text-align: center;
+}
+     .navbar-default .navbar-nav > li > a:hover{
+         background:white;
+     }
+    </style>
+	</head>
+<body style="height:100%">
+    <div id="scrollelement" class="col-md-8 col-md-offset-2" style="padding:0;">
+        <div id="tophead"><a href="/index.html"> <img id="logo" style = "height:61px" src="Images/testLogo2.png" /></a></div>
+        <nav class="navbar navbar-default " role="navigation" style="background:white; color:black; border-top:1px solid black; border-bottom: 1px solid black; border-bottom-width:95%; border-top-width:95%">
+            <div class="container-fluid" style="background:white; color:black;">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav" id="headernav">
+                        <li><a style="color:black;" href="News.html">News</a></li>
+                        <li><a style="color:black;" href="AboutUs.html">About Us</a></li>
+                        <li><a style="color:black;" href="ExistingSummits.html">Previous Summits</a></li>
+                        <li><a style="color:black;" href="Delegate.html">The Delegates</a></li>
+                        <li><a style="color:black;" href="UpcomingSummits.html">Upcoming Summits</a></li>
+                        <li><a style="color:black;" href="ContactUs.php">Contact Us</a></li>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        <div class="row">
+          <div class="col-md-7 col-md-offset-2">
+         <fieldset>
+             <legend>Contact Information</legend>
+<table>
+    <tr><td><label style="font-size: 18px">Your name:</label></td>
+        <td><input type="text" ID="nameTB" CssClass="form-inline" style="height:30px !important; width:200px"></Textbox></td>
+    </tr>
+        <tr>
+             <td><label style="font-size: 18px">Your email:</label></td>
+             <td><input type="text" ID="emailTB" CssClass="form-inline" style="height:30px !important; width:200px"></TextBox></td>
+             </tr>
+    <tr>
+         <td><label style="font-size: 18px">Phone number:</label></td>
+         <td><input type="text" ID="phoneTB" CssClass="form-inline" style="height:30px !important; width:200px"></TextBox></td>
+        </tr>
+    <tr>
+         <td><label style="font-size: 18px">Comments:</label></td>
+         <td><input type="text" CssClass="form-inline" ID="commentsTB"></TextBox></td>
+</tr>
+    </table>
+             </fieldset>
+    <table class="col-md-3 col-md-offset-3">
+         <tr> <td><input type="Submit" Text="Submit"/></td>
+        <td><input type="Reset" Text="Clear"/></td>
+    </tr>
+</table>
+    <br/>
+	</br/>
+    <p>Note: The contact page is currently being worked on. Please email mcilroy50@gmail.com to reach us during this time.</p>
+    </div>
+          <div class="row">
+          </div>
+        </div>
+    </div>
+</body>
+</html>
